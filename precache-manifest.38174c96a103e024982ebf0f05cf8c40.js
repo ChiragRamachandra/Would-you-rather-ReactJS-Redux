ac8e9874ec58275abf43ef5a97d4300afe36da85
ChiragRamachandra <chirag.ramachandra@gmail.com>
2020-06-22 15:14:11 +0530

    Updates

---
+++ precache-manifest.38174c96a103e024982ebf0f05cf8c40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d9d6ebaeffaa210198d35e98defe24",
+    "url": "/Would-you-rather-ReactJS-Redux/index.html"
+  },
+  {
+    "revision": "6bddb66f94385d0da84b",
+    "url": "/Would-you-rather-ReactJS-Redux/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "de2a98155028e1d77ac8",
+    "url": "/Would-you-rather-ReactJS-Redux/static/js/2.5984fa98.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Would-you-rather-ReactJS-Redux/static/js/2.5984fa98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bddb66f94385d0da84b",
+    "url": "/Would-you-rather-ReactJS-Redux/static/js/main.4b0622af.chunk.js"
+  },
+  {
+    "revision": "b4cdcc8f37f5be8a92b5",
+    "url": "/Would-you-rather-ReactJS-Redux/static/js/runtime-main.a6ac189e.js"
+  }
+]);
